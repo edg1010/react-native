93ce9adad7d3c3e8021b3334021370849521c8bc
edgar <102346431+edg1010@users.noreply.github.com>
2022-10-19 22:17:09 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import AppNavigation from './src/navigation';
+import {persistor, store} from './src/store';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.base}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <AppNavigation />
+        </PersistGate>
+      </Provider>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  base: {
+    display: 'flex',
+    flex: 1,
+  },
+});
+
+export default App;
